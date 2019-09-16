576996406413e47868746bdb948f316854403d8f
zhangrh <zhangrhweb@163.com>
2019-09-16 14:38:57 +0800

    更改命名方式

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:first_flutter_app/main.dart';
+// import 'package:first_flutter_app/main.dart';
+import 'package:first_flutter_app/3.2.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
