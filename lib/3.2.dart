6d3575e08c3a06008cd00611d9609622b3282c2b
zhangrh <zhangrhweb@163.com>
2019-09-17 12:09:12 +0800

    学习3.2状态管理

--- lib/3.2.dart
+++ lib/3.2.dart
@@ -1,9 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 void main() {
  runApp(MyApp()); 
 }
 class MyApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '3.2',
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('3.2'),
         ),
-        body: TapBoxA(),
+        // body: TapBoxA(),
+        body: ParentWidgetC()
       ),
     );
   }
@@ -19,11 +21,7 @@ class MyApp extends StatelessWidget {
 
 // 自身管理状态
 class TapBoxA  extends StatefulWidget{
-  const TapBoxA({
-    Key key
-  }):super(key: key);
-  @override
-  _TapBoxAState createState () => new _TapBoxAState();
+  _TapBoxAState createState () => _TapBoxAState();
 }
 
 class _TapBoxAState extends State {
@@ -57,4 +55,150 @@ class _TapBoxAState extends State {
       ),
     );
   } 
+}
+
+// 父组件管理
+
+class ParentWidget extends StatefulWidget {
+  createState () => _ParentWidgetState();
+}
+class _ParentWidgetState extends State {
+  bool _active = false;
+  void _handleTapBoxChanged (bool value) {
+    setState(() {
+      _active = value;
+    });
+  }
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapBoxB(
+        active: _active,
+        onChanged: _handleTapBoxChanged,
+      ),
+    );
+  }
+}
+
+class TapBoxB extends StatelessWidget {
+  TapBoxB({
+    Key key,
+    this.active: false,
+    @required this.onChanged
+  }):super(key: key);
+
+  final bool active;
+  final onChanged;
+
+
+  Widget build (BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        // TODO: 为什么一定要把更改状态的逻辑放到子组件, 不能放到父组件???
+        onChanged(!active);
+      },
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: new BoxDecoration(
+          color: active ? Colors.lightBlue[700] : Colors.grey[600],
+        ),
+        child: Center(
+          child: Text(
+            active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+// 混合管理
+
+
+class ParentWidgetC extends StatefulWidget{
+  _ParentWidgetCState createState () => _ParentWidgetCState();
+}
+class _ParentWidgetCState extends State {
+  bool _active = false;
+  void _handleTapboxChanged(bool val) {
+    setState(() {
+      _active = val;
+    });
+  }
+  Widget build(BuildContext context) {
+    return Container(
+      child: TapBoxC(
+        active: _active,
+        onChanged: _handleTapboxChanged,
+      ),
+    );
+  }
+}
+
+class TapBoxC extends StatefulWidget {
+  TapBoxC({
+    Key key,
+    this.active = false,
+    @required this.onChanged
+  }):super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  _TapBoxCState createState () {
+    return new _TapBoxCState();
+  }
+}
+
+// TODO: 后面这个`<>`代表什么, 为什么可以直接使用TapBoxC类中的变量
+class _TapBoxCState extends State<TapBoxC> {
+
+  bool _highlight = false;
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _highlight = true;
+    });
+  }
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _highlight = false;
+    });
+  }
+  void _handleTapCancel() {
+    setState(() {
+      _highlight = false;
+    });
+  }
+  void _handleTap() {
+    widget.onChanged(!widget.active);
+  }
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTapDown: _handleTapDown, // 处理按下事件
+      onTapUp: _handleTapUp, // 处理抬起事件
+      onTap: _handleTap,
+      onTapCancel: _handleTapCancel,
+      child: Container(
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          color: widget.active ? Colors.lightBlue[700] : Colors.grey[600],
+          border: _highlight ? Border.all(
+            color: Colors.teal[700],
+            width: 10
+          ) : null,
+        ),
+        child: Center(
+          child: Text(
+            widget.active ? 'Acitive' : 'Inactive',
+            style: TextStyle(fontSize: 32, color: Colors.white),
+          ),
+        )
+      ),
+    );
+  }
 }
