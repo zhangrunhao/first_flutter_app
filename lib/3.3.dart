141672100b8faf90f133e3b188515f3ab8b9dd37
zhangrh <zhangrhweb@163.com>
2019-09-18 11:51:30 +0800

    3.3完成: 学习字体样式与更改字体

--- lib/3.3.dart
+++ lib/3.3.dart
@@ -11,8 +11,20 @@ class MyApp extends StatelessWidget {
       title: '3.3',
       home: Scaffold(
         appBar: AppBar(title: Text('3.3'),),
-        body: TestTextClass(),
+        body: ParentText(),
       ),
+    );
+  }
+}
+
+class ParentText extends StatelessWidget {
+  Widget build (BuildContext context) {
+    return DefaultTextStyle(
+      style: TextStyle(
+        fontFamily: 'IndieFlower',
+        color: Colors.red
+      ),
+      child: TestTextClass(),
     );
   }
 }
@@ -38,8 +50,33 @@ class TestTextClass extends StatelessWidget {
           ),
           Text(
             'Hello World',
-            textScaleFactor: 1.5,
-          )
+            textScaleFactor: 1.5, // 根据系统字体大小进行缩放比例控制
+            style: TextStyle(
+              color: Colors.blue,
+              fontSize: 18, // 不会跟随系统字体大小变化
+              height: 1.2,
+              fontFamily: 'Courier',
+              // TODO: .. 是个什么写法
+              background: new Paint()..color = Colors.yellow,
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.dashed,
+            ),
+          ),
+          Text.rich(TextSpan(
+              children: [
+                TextSpan(
+                  text: 'Home: '
+                ),
+                TextSpan(
+                  text: 'https://baidu.com',
+                  style: TextStyle(
+                    color: Colors.blue
+                  ),
+                  // TODO: 未能解释这个手势操作, 最后发现必须调用dispose释放资源, 然后就必须在State类中
+                  // recognizer: _tapGestureRecognizer
+                ),
+              ]
+            )),
         ],
       )
     );
