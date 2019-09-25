c32db0bb17a7c19bc0fdaeb5748e26c40553d1b7
zhangrh <zhangrhweb@163.com>
2019-09-25 11:17:11 +0800

    完成3.7学习, 并理解context

--- lib/3/3.7.dart
+++ lib/3/3.7.dart
@@ -4,13 +4,99 @@ class TextFieldAndFormRoute extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('3.7'),),
-      body: TextFieldAndForm(),
+      body: Padding(
+        // 
+        padding: const EdgeInsets.symmetric(
+          vertical: 16.0,
+          horizontal: 24.0
+        ),
+        child: TextFieldAndForm(),
+      )
     );
   }
 }
 
 class TextFieldAndForm extends StatefulWidget {
- _TextFieldCustomStyleState createState () => _TextFieldCustomStyleState();
+ _FormState createState () => _FormState();
+}
+
+class _FormState extends State {
+
+  TextEditingController _unameController = TextEditingController();
+  TextEditingController _pwdController = TextEditingController();
+
+  // TODO: 尖括号表示什么运算
+  GlobalKey _formKey = GlobalKey<FormState>();
+
+  Widget build (BuildContext context) {
+    return Form(
+      key: _formKey, // 获取formKey
+      autovalidate: true, // 自动校验
+      child: Column(
+        children: <Widget>[
+          TextFormField( // 用户名输入框
+            autofocus: true,
+            controller: _unameController,
+            decoration: InputDecoration(
+              labelText: "用户名",
+              hintText: "用户名或邮箱",
+              icon: Icon(Icons.person)
+            ),
+            validator: (v) {
+              return v.trim().length > 0 ? null : '用户名不能为空';
+            },
+          ),
+          TextFormField( // 密码输入框
+            controller: _pwdController,
+            decoration: InputDecoration(
+              labelText: "密码",
+              hintText: "您的登录密码",
+              icon: Icon(Icons.lock)
+            ),
+            obscureText: true,
+            validator: (v) {
+              return v.trim().length > 5 ? null : "密码不能少于6位";
+            },
+          ),
+          RaisedButton(
+            child: Text("测试context"), // TextFieldAndForm(state: _FormState#585b0)
+            onPressed: () {
+              print(context);
+            },
+          ),
+          Container(
+            child: Builder(
+              /**
+               * TODO: 为什么通过这种方式构建便可以找到Form
+               * 
+               * 现在的理解:
+               * 1. 此处的Builder将return返回的widget中context返回了
+               * 2. 如果没有这个builder, 会把_FormState中的中的context返回\
+               * 
+               * 注: context是Widget操作Element的一个接口, 而widget树对应的element不同, 所以context也有所不同
+               */
+              builder: (context) { 
+                return RaisedButton( // 登录按钮
+                  child: Text("登录"),
+                  onPressed: () {
+                    print(context); // Builder(dependencies: [_FormScope])s
+                    // 不能此方法获取FromState.context
+                    // print(Form.of(context).validate());
+
+                    // 1. 通过_formKey.currentState获取FormState
+                    // 2. 调用validate()方法校验用户名和密码是否合法
+                    if ((_formKey.currentState as FormState).validate()) {
+                      print('验证通过');
+                    }
+                  },
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
 
 class _TextFieldCustomStyleState extends State {
