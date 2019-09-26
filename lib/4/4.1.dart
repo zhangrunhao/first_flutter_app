950ace25dd5a78d8b224a54158278263f98487ac
zhangrh <zhangrhweb@163.com>
2019-09-26 10:24:06 +0800

    4.1 布局类组件简介

---
+++ lib/4/4.1.dart
@@ -0,0 +1,6 @@
+/**
+ * 1. Widget的渲染基类是RenderObjectWidget
+ * 2. StatelessWidget和StatefulWidget是组合Widget的基类
+ * 3. 渲染的实现原理来自于RendObject
+ * 4. StatelessWidget和StatefulWidget并不关联最终的渲染对象RenderObjectWidget
+ */
