e835f6612a24dad1b355c69c9d1568df78778c55
zhangrh <zhangrhweb@163.com>
2020-01-07 15:50:25 +0800

    11.3学习

--- lib/11/2.dart
+++ lib/11/2.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class TestRoute extends StatelessWidget {
@@ -8,7 +9,38 @@ class TestRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: HttpTestRoute(),
+      body: TestDioRoute(),
+    );
+  }
+}
+
+class TestDioRoute extends StatefulWidget {
+  TestDioRouteState createState() => TestDioRouteState();
+}
+class TestDioRouteState extends State<TestDioRoute> {
+  Dio _dio = Dio();
+
+  @override
+  build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: FutureBuilder(
+        future: _dio.get("https://api.github.com/orgs/flutterchina/repos"),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            Response response = snapshot.data;
+            if (snapshot.hasError){
+              return Text(snapshot.error.toString());
+            }
+            return ListView(
+              children: response.data.map<Widget>((e) => 
+                ListTile(title: Text(e["full_name"]))
+              ).toList(),
+            );
+          }
+          return CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
